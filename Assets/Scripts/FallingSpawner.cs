b8de6f88b2aea9cf5f4afb574aa5e2c040eed163
adelipingu14 <adelipingu14@gmail.com>
2026-09-17 15:49:41 +0900

    feat : 낙하물 스크립트 및 충돌 판정 임시 버전 제작

--- Assets/Scripts/FallingSpawner.cs
+++ Assets/Scripts/FallingSpawner.cs
@@ -13,10 +13,20 @@ public class FallingSpawner : MonoBehaviour
 
     private void Start()
     {
+        InvokeRepeating("ObstacleRain", 0f, 1f);
+    }
 
+    void ObstacleRain()
+    { 
         float randomX = Random.Range(0f, 1f);
 
         Vector3 topEdge =
             mainCamera.ViewportToWorldPoint(new Vector3(randomX, 1.5f, 0f));
+
+        topEdge.z = 0f;
+
+        Instantiate(obstacle, topEdge, Quaternion.identity);
     }
+
+
 }
